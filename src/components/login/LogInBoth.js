dd04f7132818985288a6980124c70417951b9c1e
Mahfuzur Rahman <51076032+Mahfuzz11@users.noreply.github.com>
2022-11-29 20:57:39 +0600

    updating toast

--- src/components/login/LogInBoth.js
+++ src/components/login/LogInBoth.js
@@ -1,9 +1,11 @@
+import { ToastContainer } from 'react-toastify';
 import LogImage from '../../assets/Mobile login-cuate.svg';
 import LogLeft from './LogLeft';
 
 export default function LogInBoth() {
     return (
         <div className="container-fluid overflow-hidden mt-1" id="login-content">
+            <ToastContainer />
             <div className="container d-flex" id="full-login">
                 <LogLeft />
                 <div className="container" id="right-login">
